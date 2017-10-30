d4ce4b8f69716eb8c44ecf4870c639827a5b12e6
Jeremy Moseley <jeremy@jeremymoseley.net>
2017-10-29 22:38:33 -0700

    Add return type for secrets.

--- src/lib/secrets.ts
+++ src/lib/secrets.ts
@@ -1,10 +1,17 @@
 import * as _ from 'lodash';
-import * as asyncLib from 'async';
 import * as AWS from 'aws-sdk';
 
 const kms = new AWS.KMS();
 
-export async function resolve() {
+export interface Secrets {
+  REDDIT_CLIENT_ID: string;
+  REDDIT_CLIENT_TOKEN: string;
+  REDDIT_USERNAME: string;
+  REDDIT_PASSWORD: string;
+  STEAM_API_KEY: string;
+}
+
+export async function resolve(): Promise<Secrets> {
   console.log('Resolving secrets.');
   const secrets = await kms.decrypt({
     CiphertextBlob: Buffer.from(process.env.SECRETS, 'base64'),
