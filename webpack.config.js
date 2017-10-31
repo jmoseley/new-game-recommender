316a7eea14e0222bfc22dabd4a74ad0f0a51e8c8
Jeremy Moseley <jeremy@jeremymoseley.net>
2017-10-30 17:57:49 -0700

    Add client for searching Steam.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
+  resolveLoader: {
+    alias: {
+      'remove-hashbang-loader': path.join(__dirname, './loaders/remove-hashbang-loader')
+    }
+  },
   entry: slsw.lib.entries,
   resolve: {
     extensions: [
@@ -20,6 +25,7 @@ module.exports = {
   target: 'node',
   module: {
     loaders: [
+      { test: /JSONStream\/index.js$/, loader: 'remove-hashbang-loader' },
       { test: /\.ts(x?)$/, loader: 'ts-loader' },
     ],
   },
